2f000a5386aee1c78cbd1932b8bb50d3c4bc7067
nagomuohta <nagomuohtasecond@gmail.com>
2019-01-07 20:53:50 +0900

    Add views

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,4 +3,22 @@ class MessagesController < ApplicationController
   def index
   end
 
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def show
+  end
+
 end
