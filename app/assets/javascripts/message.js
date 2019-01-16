a1506f076549fc5d933836aed93f550967cc6cab
nagomuohta <nagomuohtasecond@gmail.com>
2019-01-16 19:58:12 +0900

    To correct

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,6 @@
 $(function(){
   function buildHTML(message){
-    var image = message.image_url !== null ? `<img src="${message.image_url}">` : '';
+    var image = message.image_url ? `<img src="${message.image_url}">` : '';
 
     var html = `<div class ="chat-main__message" >
                   <div class="chat-main__message-name" >
@@ -32,8 +32,7 @@ $(function(){
     .done(function(data){
       var html = buildHTML(data);
       $('.chat-main__body--messages-list').append(html);
-      $('.description').not(':button, :submit, :reset, :hidden') .val('')
-      $('.fa-image').not(':button, :submit, :reset, :hidden') .val('')
+      $("#submitbutton")[0].reset();
       $('.submit').attr('disabled', false);
       $('.chat-main__body').animate({scrollTop: $('.chat-main__body')[0].scrollHeight}, 'fast');
     })
